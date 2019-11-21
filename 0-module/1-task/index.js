f3f3f8057301156cc1613a3b944ad5635f50f080
Vladimir Linkov <vladimir67951@gmail.com>
2019-11-21 15:10:12 +0600

    first commit

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -4,6 +4,11 @@
  * @param {number} n index
  * @returns {number}
  */
-function sum(m, n) {
+  let m = +prompt("Введите первое число", "");
+  let n = +prompt("Введите второе число", "");
 
+function sum() {
+  alert(Math.trunc( m ** n));
 }
+
+sum();
